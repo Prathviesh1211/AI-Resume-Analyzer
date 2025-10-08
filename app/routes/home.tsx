63871ae0344c8008794cde97b194072d8bcd5a7c
Prathviesh <prathvieshnaik@gmail.com>
2025-10-09 03:30:12 +0530

    Add Navbar,Homepage

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,36 @@
+import Navbar from "~/components/Navbar";
 import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { resumes } from "~/constants";
+import ResumeCard from "~/components/ResumeCard";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Resumetrics" },
+    {
+      name: "description",
+      content: "AI-powered insights to perfect your resume.",
+    },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <Navbar />
+      <section className="main-section">
+        <div className="page-heading">
+          <h1>Track your Applications and Resume Ratings</h1>
+          <h2>Review your submissions and check AI-powered feedbacks</h2>
+        </div>
+
+      {resumes.length > 0 && (
+        <div className="resumes-section">
+          {resumes.map((resume) => (
+              <ResumeCard key={resume.id} resume={resume} />
+          ))}
+        </div>
+      )}
+      </section>
+    </main>
+  );
 }
